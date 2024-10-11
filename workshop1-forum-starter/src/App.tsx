9b7df008c2549df5431e1468d5101267ab684e56
Arslan Ametov <arsinux@yandex.com>
2024-10-11 11:04:48 -0500

    react1

--- workshop1-forum-starter/src/App.tsx
+++ workshop1-forum-starter/src/App.tsx
@@ -1,141 +1,202 @@
 import './App.scss'
 import avatar from './images/bozai.png'
+import {useState} from "react";
 
 
 // Comment List data
 const defaultList = [
-  {
-    // comment id
-    rpid: 3,
-    // user info
-    user: {
-      uid: '13258165',
-      avatar: '',
-      uname: 'Jay Zhou',
+    {
+        // comment id
+        rpid: 3,
+        // user info
+        user: {
+            uid: '13258165',
+            avatar: '',
+            uname: 'Jay Zhou',
+        },
+        // comment content
+        content: 'Nice, well done',
+        // created datetime
+        ctime: '10-18 08:15',
+        like: 88,
     },
-    // comment content
-    content: 'Nice, well done',
-    // created datetime
-    ctime: '10-18 08:15',
-    like: 88,
-  },
-  {
-    rpid: 2,
-    user: {
-      uid: '36080105',
-      avatar: '',
-      uname: 'Song Xu',
+    {
+        rpid: 2,
+        user: {
+            uid: '36080105',
+            avatar: '',
+            uname: 'Song Xu',
+        },
+        content: 'I search for you thousands of times, from dawn till dusk.',
+        ctime: '11-13 11:29',
+        like: 88,
     },
-    content: 'I search for you thousands of times, from dawn till dusk.',
-    ctime: '11-13 11:29',
-    like: 88,
-  },
-  {
-    rpid: 1,
-    user: {
-      uid: '30009257',
-      avatar,
-      uname: 'John',
+    {
+        rpid: 1,
+        user: {
+            uid: '30009257',
+            avatar,
+            uname: 'John',
+        },
+        content: 'I told my computer I needed a break... now it will not stop sending me vacation ads.',
+        ctime: '10-19 09:00',
+        like: 66,
     },
-    content: 'I told my computer I needed a break... now it will not stop sending me vacation ads.',
-    ctime: '10-19 09:00',
-    like: 66,
-  },
 ]
 // current logged in user info
 const user = {
-  // userid
-  uid: '30009257',
-  // profile
-  avatar,
-  // username
-  uname: 'John',
+    // userid
+    uid: '30009257',
+    // profile
+    avatar,
+    // username
+    uname: 'John',
 }
 
 // Nav Tab
 const tabs = [
-  { type: 'hot', text: 'Top' },
-  { type: 'newest', text: 'Newest' },
+    {type: 'hot', text: 'Top', active: true},
+    {type: 'newest', text: 'Newest', active: false},
 ]
 
+type Post = {
+    rpid: number,
+    user: {
+        uid: string,
+        avatar: string,
+        uname: string,
+    },
+    content: string,
+    ctime: string,
+    like: number,
+}
+
 const App = () => {
-  return (
-    <div className="app">
-      {/* Nav Tab */}
-      <div className="reply-navigation">
-        <ul className="nav-bar">
-          <li className="nav-title">
-            <span className="nav-title-text">Comments</span>
-            {/* Like */}
-            <span className="total-reply">{10}</span>
-          </li>
-          <li className="nav-sort">
-            {/* highlight class name： active */}
-            <span className='nav-item'>Top</span>
-            <span className='nav-item'>Newest</span>
-          </li>
-        </ul>
-      </div>
+    const [mytabs, setmytabs] = useState(tabs);
+    const [list, setList] = useState<Post[]>(defaultList);
+    const [post, setPost] = useState('');
 
-      <div className="reply-wrap">
-        {/* comments */}
-        <div className="box-normal">
-          {/* current logged in user profile */}
-          <div className="reply-box-avatar">
-            <div className="bili-avatar">
-              <img className="bili-avatar-img" src={avatar} alt="Profile" />
-            </div>
-          </div>
-          <div className="reply-box-wrap">
-            {/* comment */}
-            <textarea
-              className="reply-box-textarea"
-              placeholder="tell something..."
-            />
-            {/* post button */}
-            <div className="reply-box-send">
-              <div className="send-text">post</div>
-            </div>
-          </div>
-        </div>
-        {/* comment list */}
-        <div className="reply-list">
-          {/* comment item */}
-          <div className="reply-item">
-            {/* profile */}
-            <div className="root-reply-avatar">
-              <div className="bili-avatar">
-                <img
-                  className="bili-avatar-img"
-                  alt=""
-                />
-              </div>
+    function toggleNav(v: string) {
+        const newTabs = mytabs.map((tab) => {
+            tab.active = tab.type === v;
+            return tab;
+        });
+        setmytabs(newTabs);
+        setList(list.sort((a, b) => {
+            if (v === 'hot') {
+                return b.like - a.like;
+            } else {
+                return new Date(b.ctime).getTime() - new Date(a.ctime).getTime();
+            }
+        }));
+    }
+
+    function deletePost(rpid: number) {
+        const newList = list.filter((item) => item.rpid !== rpid);
+        setList(newList);
+    }
+
+    function createPost() {
+        const newPost:Post = {
+            rpid: list.length + 1,
+            user: user,
+            content: post,
+            ctime: new Date().toLocaleString(),
+            like: 0,
+        }
+        const newList = [newPost, ...list];
+        setList(newList);
+        setPost('');
+    }
+
+    return (
+        <div className="app">
+            {/* Nav Tab */}
+            <div className="reply-navigation">
+                <ul className="nav-bar">
+                    <li className="nav-title">
+                        <span className="nav-title-text">Comments</span>
+                        {/* Like */}
+                        <span className="total-reply">{list.length}</span>
+                    </li>
+                    <li className="nav-sort">
+                        {mytabs.map((tab) => {
+                            return (
+                                <span
+                                    key={tab.type}
+                                    className={`nav-item ${tab.active ? 'active' : ''}`}
+                                    onClick={() => toggleNav(tab.type)}
+                                >
+                    {tab.text}
+                    </span>)
+                        })}
+                    </li>
+                </ul>
             </div>
 
-            <div className="content-wrap">
-              {/* username */}
-              <div className="user-info">
-                <div className="user-name">jack</div>
-              </div>
-              {/* comment content */}
-              <div className="root-reply">
-                <span className="reply-content">This is reply</span>
-                <div className="reply-info">
-                  {/* comment created time */}
-                  <span className="reply-time">{'2023-11-11'}</span>
-                  {/* total likes */}
-                  <span className="reply-time">Like:{100}</span>
-                  <span className="delete-btn">
-                    Delete
-                  </span>
+            <div className="reply-wrap">
+                {/* comments */}
+                <div className="box-normal">
+                    {/* current logged in user profile */}
+                    <div className="reply-box-avatar">
+                        <div className="bili-avatar">
+                            <img className="bili-avatar-img" src={user.avatar} alt="Profile"/>
+                        </div>
+                    </div>
+                    <div className="reply-box-wrap">
+                        {/* comment */}
+                        <textarea
+                            value={post}
+                            onChange={(e) => setPost(e.target.value)}
+                            className="reply-box-textarea"
+                            placeholder="tell something..."
+                        />
+                        {/* post button */}
+                        <div className="reply-box-send" onClick={createPost}>
+                            <div className="send-text">post</div>
+                        </div>
+                    </div>
                 </div>
-              </div>
+                {/* comment list */}
+                {list.map((item) => {
+                    return (
+                        <div className="reply-list" key={item.rpid}>
+                            {/* comment item */}
+                            <div className="reply-item">
+                                {/* profile */}
+                                <div className="root-reply-avatar">
+                                    <div className="bili-avatar">
+                                        <img
+                                            className="bili-avatar-img"
+                                            alt=""
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="content-wrap">
+                                    {/* username */}
+                                    <div className="user-info">
+                                        <div className="user-name">{item.user.uname}</div>
+                                    </div>
+                                    {/* comment content */}
+                                    <div className="root-reply">
+                                        <span className="reply-content">{item.content}</span>
+                                        <div className="reply-info">
+                                            {/* comment created time */}
+                                            <span className="reply-time">{item.ctime}</span>
+                                            {/* total likes */}
+                                            <span className="reply-time">Like:{item.like}</span>
+                                            <span className="delete-btn" onClick={() => deletePost(item.rpid)}>Delete</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App
